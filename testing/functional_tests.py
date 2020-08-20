5ccbc454392d126ed6f88ab46111644c5f3e83b6
Adam Worwood <adamdworwood@gmail.com>
2020-08-20 12:02:53 +0100

    baby steps

--- testing/functional_tests.py
+++ testing/functional_tests.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Open the web page
+        self.browser.get('http://127.0.0.1:8000')
+
+        # Header is "CS Assignment"
+        self.assertIn('CS Assignment', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
+
+browser.quit()
