e83f583b24f1b0314feb77753239a54801188c73
Adam Worwood <adamdworwood@gmail.com>
2020-08-20 13:31:52 +0100

    not sure if the lists folder is necessary but i'm just following the tutorial for now

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name = 'post_list'),
+    url(r'^$', views.home_page, name='home'),
+    path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit')
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
-# Create your views here.
+def home_page(request):
+    pass
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
